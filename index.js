39e49c7abff8149cf6c8e0bc88525c4796fb996a
chethancm2001 <chethancm2001kadur@gmail.com>
2022-04-15 09:23:00 +0530

    added monogodb

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const mogoose = require('mongoose')
 
 const app = express()
+mogoose.connect('mongodb+srv://chethancm001:chethancm@cluster0.2p5zc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+.then(()=>{
+    console.log('conected to db')
+}).catch(()=>{console.log('connected to db')})
+
+
+app.use(express.json())
+app.use(express.urlencoded())
 
 app.post('/',(req,res)=>{
-    res.send(req.body)
+    let body =req.body
+    res.send(body)
 })
+
 app.get('/sensor-data',(req,res)=>{
     res.send("working fine")
 })
