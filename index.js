7c7226fab4586a1aad71f38947fd4285bb28418c
Ajith <esssrvfls@zohomail.in>
2022-04-15 08:51:02 +0530

    updated with another route

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
-
 const app = express()
 
+app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req,res) => {
+    // Query the db and pass it to home.ejs to display or render a Landing page
+    res.send('I am up')
+})
+
 app.post('/',(req,res)=>{
+    // Data is recieved in url-encoded form. Should be added to db
     res.send(req.body)
 })
+
 app.get('/sensor-data',(req,res)=>{
     res.send("working fine")
 })
