c0d39d7fcad6b8de734984d632bc2d2892a47091
chethancm2001 <chethancm2001kadur@gmail.com>
2022-04-16 11:31:23 +0530

    added twilo

--- routing.js
+++ routing.js
@@ -1,17 +1,31 @@
 const express = require('express')
+const client = require('twilio')("AC753703eee11db4feddc2c6bbbf00912b", "25132e0b4b1e51d08390085070673d17");
 const Datamodel = require("./model.js")
-routes = express.Router()
 
+
+routes = express.Router()
+client.logLevel = 'debug';
 //all the router  come here 
 
+
+function sendsms(mess){
+  client.messages.create({
+      body:mess,
+      from:"+13256670177",
+      to:"+919148521359"
+  }).then(message=>{
+      console.log(message)
+  }).catch(err=>{console.log(err)})
+  }
+
+  
+  
 
 //home page route
 routes.get('/',(req,res)=>{
     async function datafromdb(){
         let data1 = await Datamodel.find({})
         res.send(data1)
-        
-      
         }
         datafromdb()
 })
@@ -37,6 +51,17 @@ routes.post('/post',(req,res)=>{
     var ledd = Number(req.body.led);
     console.log([fired,gasd,waterd,ledd])
    //validation and twilio came here
+   //twilio
+   if(fired===1){
+     sendsms("fire is detected in home")
+   }
+   if(gasd === 1){
+     sendsms("gas is leaking in home")
+   }
+   if(waterd===1){
+     sendsms("water is full in home")
+   }
+   
     async function datatodb(){
         let datan = new Datamodel({firedata:fired,gasdata:gasd,waterlevel:waterd,led:ledd})
         let result =await  datan.save()
