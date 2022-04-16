7a1a16e5876ae3815268787cceff94f2affad268
chethancm2001 <chethancm2001kadur@gmail.com>
2022-04-16 10:01:37 +0530

    added

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
+//importing the packages
 const express = require('express')
-
 const mogoose = require('mongoose')
 const routes = require('./routing.js')
-require('dotenv').config()
+const cros = require('cors')
 
 const app = express()
 
 //connecting monogdb
-mogoose.connect(process.env.DB_URL)
+mogoose.connect("mongodb+srv://chethancm001:chethancm@cluster0.2p5zc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 .then(()=>{
     console.log('conected to db')
 }).catch(()=>{console.log('connected to db')})
 
-
+//midlewares
+app.use(cros())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 //ejs setup
 
 
+//all the routes goes here
 app.use('/',routes)
 
 
@@ -26,7 +28,7 @@ app.use('/',routes)
 
 const port = process.env.PORT || 3000
 
-
+//setting the server
 app.listen(port,()=>{
     console.log('server is runing')
 })
--- routing.js
+++ routing.js
@@ -2,34 +2,54 @@ const express = require('express')
 const Datamodel = require("./model.js")
 routes = express.Router()
 
-//all the router 
+//all the router  come here 
 
 
 //home page route
 routes.get('/',(req,res)=>{
     async function datafromdb(){
         let data1 = await Datamodel.find({})
-        console.log(data1)
-        res.json({data:"data1"})
+        res.send(data1)
+        
       
         }
         datafromdb()
 })
 
 //update page route
-routes.get('/post',(req,res)=>{
+routes.post('/post',(req,res)=>{
 //input datavalidation
 
-async function updatadb(){
-  let resu = await Datamodel.findByIdAndUpdate("625908d378ae67ace32a6685",{
-      $set:{
-          firedata:0
-          }
-  })
-  console.log(resu)
-}
-updatadb() 
-res.send("updated")
+
+// async function updatadb(){
+//   let resu = await Datamodel.findByIdAndUpdate("625908d378ae67ace32a6685",{
+//       $set:{
+//           firedata:0
+//           }
+//   })
+//   console.log(resu)
+// }
+// updatadb() 
+// res.send("updated")
+    var fired = Number(req.body.fire);
+    var  gasd=  Number(req.body.gas);
+    var waterd = Number(req.body.water);
+    var ledd = Number(req.body.led);
+    console.log([fired,gasd,waterd,ledd])
+   //validation and twilio came here
+    async function datatodb(){
+        let datan = new Datamodel({firedata:fired,gasdata:gasd,waterlevel:waterd,led:ledd})
+        let result =await  datan.save()
+        if(result){
+            res.send(result)
+        }
+        else{
+          res.send('not updated')
+        }
+    }
+    datatodb()
+
+
 
 })
 
